dca5221a2982095f02abbe763c5c4302a5255ab9
alineprasser <aline_bravin@hotmail.com>
2018-05-06 22:22:37 -0300

    Add files via upload

---
+++ Escrever_NFC/Escrever_NFC.ino
@@ -0,0 +1,40 @@
+#if 1
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+#else 0
+
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#endif
+
+void setup() {
+      Serial.begin(9600);
+      Serial.println("Escreva em uma tag NFC!\nAlterações no código por Aline Bravin Prasser");
+      nfc.begin();
+}
+
+void loop() {
+    Serial.println("\nAproxime uma tag NFC do leitor!");
+    if (nfc.tagPresent()) {
+        NdefMessage message = NdefMessage();
+        message.addUriRecord(" "); //INSIRA DENTRO DAS ASPAS A MENSAGEM QUE DESEJA GRAVAR!
+
+        bool success = nfc.write(message);
+        if (success) {
+          Serial.println("Mensagem gravada com sucesso!");        
+        } else {
+          Serial.println("Erro ao inserir mensagem.");
+        }
+    }
+    delay(5000);
+}
---
+++ Ler_NFC/Ler_NFC.ino
@@ -0,0 +1,83 @@
+#if 1
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+#else 0 
+
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#endif
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Leitor NFC!\nAlterações no código por Aline Bravin Prasser");
+  nfc.begin();
+}
+
+void loop(void) {
+  Serial.println("\nAproxime uma tag NFC ao leitor.\n");
+
+  if (nfc.tagPresent())
+  {
+    NfcTag tag = nfc.read();
+    Serial.println(tag.getTagType());
+    Serial.print("UID: ");Serial.println(tag.getUidString());
+
+    if (tag.hasNdefMessage()) // caso a tag não tenha uma mensagem gravada
+    {
+
+      NdefMessage message = tag.getNdefMessage();
+      Serial.print("\nMensagem desta tag: ");
+      Serial.print(message.getRecordCount());
+      Serial.print(" Mensagem");
+      if (message.getRecordCount() != 1) {
+        Serial.print("(s)");
+      }
+      Serial.println(".");
+
+      // loop entre as mensagens, imprimindo cada uma delas
+      int recordCount = message.getRecordCount();
+      for (int i = 0; i < recordCount; i++)
+      {
+        Serial.print("\nMensagem gravada na tag: ");Serial.println(i+1);
+        NdefRecord record = message.getRecord(i);
+
+        Serial.print("  TNF: ");Serial.println(record.getTnf());
+        Serial.print("  Tipo: ");Serial.println(record.getType()); // será "" para TNF_EMPTY
+
+        int payloadLength = record.getPayloadLength();
+        byte payload[payloadLength];
+        record.getPayload(payload);
+
+        // Print the Hex and Printable Characters
+        Serial.print("  Payload (HEX): ");
+        PrintHexChar(payload, payloadLength);
+
+        // Force the data into a String (might work depending on the content)
+        // Real code should use smarter processing
+        String payloadAsString = "";
+        for (int c = 0; c < payloadLength; c++) {
+          payloadAsString += (char)payload[c];
+        }
+        Serial.print("  Payload (as String): ");
+        Serial.println(payloadAsString);
+
+        // id is probably blank and will return ""
+        String uid = record.getId();
+        if (uid != "") {
+          Serial.print("  ID: ");Serial.println(uid);
+        }
+      }
+    }
+  }
+  delay(3000);
+}
