769ac75d27c5a98f18c86097ed374d24b44f817c
alineprasser <aline_bravin@hotmail.com>
2018-06-08 20:34:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,26 @@ def confereop(op):
 		i = 1
 		presenca = LeitorNfcPython.main()
 		arquivo = open('chamada%d.txt'%(i),'w')
-		i += 2
+		i += 1
 		arquivo.write("Relatório de presença:\n")
 		arquivo.write(datetime.datetime.now().strftime("%d/%m/%y às %H:%M:%S\n"))
 		for i in range(len(presenca)):
 			arquivo.write("%d - %s\n"%(i+1,presenca[i]))
 		arquivo.close()
-		#print(presenca)
+		
 		op = int(input("\nInsira para continuar:\n1 - Realizar chamada novamente\n0 - Voltar para o menu inicial\n"))
 		while not(op == 1 or op == 0):
 			op = int(input("Operação inválida! Informe uma operação indicada.\n"))
 		while(op == 1):
 			cls()
 			presenca = LeitorNfcPython.main()
-			#print(presenca)
+			arquivo = open('chamada%d.txt'%(i),'w')
+			i += 1
+			arquivo.write("Relatório de presença:\n")
+			arquivo.write(datetime.datetime.now().strftime("%d/%m/%y às %H:%M:%S\n"))
+			for i in range(len(presenca)):
+				arquivo.write("%d - %s\n"%(i+1,presenca[i]))
+			arquivo.close()
 			op = int(input("\nInsira para continuar:\n1 - Realizar chamada novamente\n0 - Voltar para o menu inicial\n"))
 	else:
 		ifcbanco.main()
